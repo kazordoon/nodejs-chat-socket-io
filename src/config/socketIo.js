a2c580590d970a86718dec1b6eecbd790508c6dc
Felipe Barros <felipetech@tuta.io>
2020-03-23 12:07:59 -0300

    chore: Add sending and receiving messages

--- src/config/socketIo.js
+++ src/config/socketIo.js
@@ -1,9 +1,14 @@
+/* eslint-disable no-console */
 module.exports = (server) => {
   // eslint-disable-next-line global-require
   const io = require('socket.io').listen(server);
 
   io.on('connection', (socket) => {
-    // eslint-disable-next-line no-console
     console.log(`Socket connected: ${socket.id}`);
+
+
+    socket.on('sendMessage', (data) => {
+      socket.broadcast.emit('receiveMessage', data);
+    });
   });
 };
--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -1,1 +1,46 @@
-const socket = io('http://localhost:3333');
+const socket = io.connect('http://localhost:3333');
+
+socket.on('receiveMessage', renderMessage);
+
+const form = document.forms['chat'];
+
+form.addEventListener('submit', handleChatForm);
+
+function handleChatForm(event) {
+  event.preventDefault();
+
+  const username = document.querySelector('input#username');
+  const message = document.querySelector('input#messageContent');
+
+  if (!username.value.length || !message.value.length) return;
+
+  const messageObject = {
+    username: username.value,
+    message: message.value
+  };
+
+  socket.emit('sendMessage', messageObject);
+
+  renderMessage(messageObject);
+
+  message.value = '';
+  message.focus();
+}
+
+function renderMessage({ username, message }) {
+  const messagesContainer = document.querySelector('div#messages');
+  const messageContainer = document.createElement('div');
+
+  const usernameEl = document.createElement('strong');
+  const usernameText = document.createTextNode(username + ': ');
+  usernameEl.appendChild(usernameText);
+
+  const messageEl = document.createElement('span');
+  const messageText = document.createTextNode(message);
+  messageEl.appendChild(messageText);
+
+  messageContainer.appendChild(usernameEl);
+  messageContainer.appendChild(messageEl);
+  
+  messagesContainer.appendChild(messageContainer);
+}
